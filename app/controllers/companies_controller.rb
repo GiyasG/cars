dc9c03611df4b3c5a5feeebccb4a293104fff3db
User1 <C:\Users\Embassy\AppData\Roaming\The Bat!>
2018-04-11 16:32:16 +0300

    css changes

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -62,7 +62,7 @@ class CompaniesController < ApplicationController
     authorize @company
     respond_to do |format|
       if @company.update(company_params)
-        format.html { redirect_to @company, notice: 'Company was successfully updated.' }
+        format.html { redirect_to companies_path, notice: 'Company was successfully updated.' }
         format.json { render :show, status: :ok, location: @company }
       else
         format.html { render :edit }
